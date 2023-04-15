c536736402c50417346682a6b4cf5e2709fb9544
Ernest <em123nest@gmail.com>
2023-04-15 14:06:08 +0100

    python dsa

---
+++ main/number_of_occurrences.py
@@ -0,0 +1,7 @@
+def find_occurrences():
+    word = "MISSISSIPPI"
+
+    count_for_s = word.count('s')
+    count_for_i = word.count('i')
+    print("Number of 's': " + str(count_for_s))
+    print("Number of 'i': " + str(count_for_i))
