b22c77d24a5129a3627a1f00923b3d0b8f7e8a81
Ernest <em123nest@gmail.com>
2023-04-15 03:48:51 +0100

    python dsa

---
+++ main/print_word_on_separate_line.py
@@ -0,0 +1,8 @@
+def print_out_the_word_semicolon_on_a_separate_line():
+    word_semicolon = "SEMICOLON"
+    for every_word in word_semicolon:
+        print(every_word)
+
+
+if __name__ == '__main__':
+    print(print_out_the_word_semicolon_on_a_separate_line())
