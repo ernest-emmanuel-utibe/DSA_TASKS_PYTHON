60789eb37f2f5114965c74b61aa8073b12fa188c
Ernest Emmanuel Utibe <110993733+morsecodes007@users.noreply.github.com>
2023-05-30 02:10:07 +0100

    Update print_all_even_numbers.py

--- level_3/print_all_even_numbers.py
+++ level_3/print_all_even_numbers.py
@@ -1,8 +1,7 @@
 nums = range(1, 1000)
 
 
-# create a function called is_prime that takes a single number that returns false when the number is not prime
-# but returns true if the number is prime
+# create a function called is_prime that takes a single number that returns false when the number is not prime but returns true if the number is prime
 def is_prime(num):
     for x in range(2, num):
         if (num % x) == 0:
