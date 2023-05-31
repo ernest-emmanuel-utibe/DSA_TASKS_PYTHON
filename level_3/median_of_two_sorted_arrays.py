6cffa5b0621ca0efdaada16ade2ab0e98f41efe9
Ernest Emmanuel Utibe <110993733+morsecodes007@users.noreply.github.com>
2023-05-31 16:53:39 +0100

    Update median_of_two_sorted_arrays.py

--- level_3/median_of_two_sorted_arrays.py
+++ level_3/median_of_two_sorted_arrays.py
@@ -1,8 +1,6 @@
-#        Given two sorted arrays nums1 and nums2 of size m and n respectively,
-#        return the median of the two sorted arrays.The overall run time complexity should be O(log (m+n)).
+#        Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.The overall run time complexity should be O(log (m+n)).
 
 def find_median_sorted(nums1, nums2):
-
 
 n1 = len(nums1)
 n2 = len(nums2)
